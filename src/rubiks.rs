ab994b62ef8ed461fd3fb4ae6251a5ff435297f8
ryan-way <ryan.keith.way@gmail.com>
2022-08-15 15:11:59 -0700

    Added simple api for rubiks cube move

--- src/rubiks.rs
+++ src/rubiks.rs
@@ -1,3 +1,4 @@
+use crate::transforms::*;
 
 #[derive(PartialEq, Clone, Copy, Debug)]
 pub enum RubiksCell {
@@ -22,7 +23,7 @@ impl RubiksCell {
             [
                 [YB, Y, YG],
                 [B, C, G],
-                [BW, B, GW]
+                [BW, W, GW]
             ],
             [
                 [YBO, YO, YGO],
@@ -55,6 +56,35 @@ impl RubiksCube3x3 {
         }
     }
 
+    fn single_u(&mut self) {
+        let mut plane = get_x_z_plane(&mut self.cube, 0);
+        rotate90(&mut plane);
+        set_x_z_plane(&mut self.cube, &plane, 0);
+    }
+
+    fn single_r(&mut self) {
+        let mut plane = get_y_z_plane(&mut self.cube, 2);
+        rotate90(&mut plane);
+        set_y_z_plane(&mut self.cube, &plane, 2);
+    }
+
+    fn single_f(&mut self) {
+        let mut plane = get_x_y_plane(&mut self.cube, 0);
+        rotate90(&mut plane);
+        set_y_z_plane(&mut self.cube, &plane, 0);
+    }
+
+    fn single_d(&mut self) {
+        let mut plane = get_x_z_plane(&mut self.cube, 2);
+        rotate90(&mut plane);
+        set_x_z_plane(&mut self.cube, &plane, 2);
+    }
+
+    fn single_u_prime(&mut self) {
+        let mut plane = get_x_z_plane(&mut self.cube, 0);
+        rotate_minus90(&mut plane);
+        set_x_z_plane(&mut self.cube, &plane, 0);
+    }
 }
 
 pub trait RubiksCube {
@@ -90,6 +120,26 @@ mod tests {
 
     #[test]
     fn rubiks_cube_not_solved_when_modified() {
-        let rc = RubiksCube3x3::new();
+        let mut rc = RubiksCube3x3::new();
+
+        rc.single_u();
+
+        assert_eq!(false, rc.solved());
+
+        rc.single_u_prime();
+
+        assert_eq!(true, rc.solved());
     }
+
+    #[test]
+    fn single_u_solves_if_repeated_four_times() {
+        let mut rc = RubiksCube3x3::new();
+
+        for _ in (0..4) {
+            rc.single_u();
+        }
+
+        assert_eq!(true, rc.solved());
+    }
+
 }
